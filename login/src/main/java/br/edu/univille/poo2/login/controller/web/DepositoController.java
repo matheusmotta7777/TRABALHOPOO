d667b13be120162e4c918c9c711e087f74d35954
matheusmotta7777 <matheus.motta@univille.br>
2024-11-25 22:00:55 -0300

    Delete login directory

--- login/src/main/java/br/edu/univille/poo2/login/controller/web/DepositoController.java
+++
@@ -1,4 +0,0 @@
-package br.edu.univille.poo2.login.controller.web;
-
-public class DepositoController {
-}
--- login/src/main/java/br/edu/univille/poo2/login/core/repository/UserRoleRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.univille.poo2.login.core.repository;
-
-import br.edu.univille.poo2.login.core.entity.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-}
